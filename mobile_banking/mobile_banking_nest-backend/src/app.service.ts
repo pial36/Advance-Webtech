390ccb2d996b2d5f43383673efb831d5d5cb2e9d
Pial Hassan Chowdhury <pialhassanchowdhury@gmail.com>
2024-05-18 21:25:57 +0600

    ok

--- mobile_banking/mobile_banking_nest-backend/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hey Developer! Cool Down, get a Coffee, Your NestJS is working fine.';
-  }
-}
